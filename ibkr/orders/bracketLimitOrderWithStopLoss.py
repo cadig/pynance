30538abf4eb2d5343365d4cdcc381cd4db53431c
John Cadigan <33014669+cadig@users.noreply.github.com>
2025-02-22 09:25:20 -0700

    update orders/bracketLimitOrder.py

--- ibkr/orders/bracketLimitOrderWithStopLoss.py
+++ ibkr/orders/bracketLimitOrderWithStopLoss.py
@@ -1,8 +1,8 @@
 from ib_insync import Order
 
-def bracketLimitOrder(
+def bracketLimitOrderWithStopLoss(
         accountId: str, parentOrderId:int, tif: str, action:str, quantity:int, 
-        limitPrice:float, takeProfitLimitPrice:float, stopLossPrice:float
+        limitPrice:float, stopLossPrice:float
     ):
 
      parent = Order()
@@ -17,19 +17,19 @@ def bracketLimitOrder(
      #The LAST CHILD will have it set to True, 
      parent.transmit = False
 
-     takeProfit = Order()
-     takeProfit.account = accountId
-     takeProfit.orderId = parent.orderId + 1
-     takeProfit.action = "SELL" if action == "BUY" else "BUY"
-     takeProfit.orderType = "LMT"
-     takeProfit.totalQuantity = quantity
-     takeProfit.lmtPrice = takeProfitLimitPrice
-     takeProfit.parentId = parentOrderId
-     takeProfit.transmit = False
+    #  takeProfit = Order()
+    #  takeProfit.account = accountId
+    #  takeProfit.orderId = parent.orderId + 1
+    #  takeProfit.action = "SELL" if action == "BUY" else "BUY"
+    #  takeProfit.orderType = "LMT"
+    #  takeProfit.totalQuantity = quantity
+    #  takeProfit.lmtPrice = takeProfitLimitPrice
+    #  takeProfit.parentId = parentOrderId
+    #  takeProfit.transmit = False
 
      stopLoss = Order()
      stopLoss.account = accountId
-     stopLoss.orderId = parent.orderId + 2
+     stopLoss.orderId = parent.orderId + 1
      stopLoss.action = "SELL" if action == "BUY" else "BUY"
      stopLoss.orderType = "STP"
      #Stop trigger price
@@ -40,5 +40,5 @@ def bracketLimitOrder(
      #to activate all its predecessors
      stopLoss.transmit = True
 
-     bracketOrder = [parent, takeProfit, stopLoss]
+     bracketOrder = [parent, stopLoss]
      return bracketOrder
